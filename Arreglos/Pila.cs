1b511287f92de846521b39b8186e11686481fd88
hdenogean <hdenogeancruz@gmail.com>
2025-09-27 13:05:26 -0700

    implementando el metodo agregar

---
+++ Arreglos/Pila.cs
@@ -0,0 +1,25 @@
+﻿namespace Arreglos
+{
+    internal class Pila
+    {
+        string[] _arreglo;
+        int _tope;
+        public Pila(int elementos)
+        {
+            _arreglo = new string[elementos];
+            _tope = 0;
+        }
+
+        public void Agregar(string dato)
+        {
+            int max = _arreglo.Length - 1;
+            if (_tope > max)
+            {
+                throw new Exception("Pila llena"); 
+            }
+
+            _arreglo[_tope] = dato;
+            _tope++;
+        }
+    }
+}
---
+++ Arreglos/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Arreglos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Pila pila = new Pila(5);
+
+                pila.Agregar("A");
+                pila.Agregar("B");
+                pila.Agregar("C");
+                pila.Agregar("D");
+                pila.Agregar("E");
+                pila.Agregar("F");
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+
+    }
+}
